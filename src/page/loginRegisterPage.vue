24a06848c5022a965e61b40a5cdd23d4b2d30201
MuWei-CN <137994544+MuWei-CN@users.noreply.github.com>
2025-04-27 20:08:12 +0800

    调用后端接口

--- src/page/loginRegisterPage.vue
+++ src/page/loginRegisterPage.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref, reactive } from 'vue'
-import { ElMessage } from 'element-plus'
+import { ElMessage, ElMessageBox} from 'element-plus'
+import axios from "axios";
+import {RequestHeader} from "@/store/RequestHeader.js";
 
 // 登录表单数据
 const loginForm = reactive({
@@ -20,10 +22,56 @@ const registerForm = reactive({
 
 // 控制显示登录还是注册表单
 const showLogin = ref(true)
+// 登录/注册按钮禁用状态
+const isDisabled = ref(false)
+// 控制对话框显示
+const dialogVisible = ref(false)
+// 控制对话框内容
+const dialogContent = ref("")
 
 // 切换登录/注册表单
 const toggleForm = () => {
   showLogin.value = !showLogin.value
+}
+
+// 登录处理
+const handleLogin = async () => {
+  if (!loginForm.username || !loginForm.password) {
+    ElMessage.error('请填写用户名和密码')
+    return
+  }
+
+  isDisabled.value = true
+  // TODO 调用登录接口
+  axios({
+    url: 'http://localhost:8080/api/user/login',
+    headers:{
+      'Content-Type': "application/x-www-form-urlencoded",
+    },
+    method: 'post',
+    data: {
+      username: loginForm.username,
+      password: loginForm.password
+    }
+  }).then(res => {
+    let resdata = res.data
+    if (resdata.code === 200){
+      ElMessage.success("登录成功")
+      RequestHeader.Authorization = resdata.data
+    }else if(resdata.code === 460){
+      ElMessage.error("用户不存在")
+    }else if(resdata.code === 461){
+      ElMessage.error("用户密码错误")
+    }else{
+      ElMessage.error("未知错误")
+    }
+  }).catch(error =>{
+    // 处理错误
+    dialogContent.value = error
+    dialogVisible.value = true
+  }).finally(() => {
+    isDisabled.value = false
+  })
 }
 
 // 注册处理
@@ -44,10 +92,63 @@ const handleRegister = async () => {
     return
   }
   // TODO: 调用注册接口
+  isDisabled.value = true
+  const formData = new FormData()
+  formData.append("username", registerForm.username)
+  formData.append("password", registerForm.password)
+  formData.append("user_type","PERSONAL")
+  formData.append("mobile", registerForm.phone)
+  if(registerForm.email) formData.append("email", registerForm.email)
+  if(registerForm.gender) formData.append("gender", registerForm.gender)
+  if(registerForm.realName) formData.append("real_name", registerForm.realName)
+  axios({
+    url: 'http://localhost:8080/api/user/register',
+    headers:{
+      'Content-Type': "multipart/form-data",
+    },
+    method: 'post',
+    data: formData
+  }).then(res => {
+    let resdata = res.data
+    if (resdata.code === 200){
+      RequestHeader.Authorization = resdata.data
+      ElMessage.success("注册成功")
+      showLogin.value = true
+    }else if(resdata.code === 440){
+      ElMessage.error("用户名已存在")
+    }else if(resdata.code === 441){
+      ElMessage.error("手机号已存在")
+    }else if(resdata.code === 442){
+      ElMessage.error("邮箱已存在")
+    }else{
+      ElMessage.error("未知错误")
+    }
+  }).catch(error => {
+    dialogContent.value = error
+    dialogVisible.value = true
+  }).finally(() => {
+    isDisabled.value = false
+  })
 }
 </script>
 
 <template>
+  <el-dialog
+      v-model="dialogVisible"
+      title="系统错误信息"
+      width="500"
+  >
+    <span>{{ dialogContent }}</span>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">
+          确认
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
+
   <div class="auth-container">
     <el-card>
       <div v-if="showLogin">
@@ -59,7 +160,7 @@ const handleRegister = async () => {
           <el-form-item label="密码" style="width: 90%">
             <el-input v-model="loginForm.password" type="password" placeholder="请输入密码" />
           </el-form-item>
-          <el-button type="primary" @click="handleLogin">登录</el-button>
+          <el-button type="primary" @click="handleLogin" :disabled="isDisabled">登录</el-button>
         </el-form>
         <p class="switch-form">
           还没有账号？
@@ -92,7 +193,7 @@ const handleRegister = async () => {
           <el-form-item label="邮箱" style="width: 90%">
             <el-input v-model="registerForm.email" placeholder="请输入邮箱" />
           </el-form-item>
-          <el-button type="primary" @click="handleRegister">注册</el-button>
+          <el-button type="primary" @click="handleRegister" :disabled="isDisabled">注册</el-button>
         </el-form>
         <p class="switch-form">
           已有账号？
